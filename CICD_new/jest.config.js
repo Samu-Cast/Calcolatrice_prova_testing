14d0ea7cc16271f6368db6280baa64d6342bfc1b
Samucast04 <samuele.castellani@student.univaq.it>
2025-12-05 12:55:23 +0100

    Fix: Configurazione Jest per CI/CD

---
+++ CICD_new/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: 'node',
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    testMatch: ['**/test/**/*.test.js'],
+    collectCoverage: false,
+    verbose: true
+};
